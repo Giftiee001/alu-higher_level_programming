156e2ab033fc99044417f8696682cc4a41752eb4
Gift <g.donemmanu@alustudent.com>
2024-02-11 05:33:48 -0800

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-// Writing a script that reads and prints the content of a file
-const fs = require('fs');
 
-fs.readFile(process.argv[2], (error, content) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(content.toString());
-  }
-}
+const request = require('fs');
+
+const filePath = process.argv[2];
+
+request.readFile(filePath, function (err, data) {
+  if (err) console.log(err);
+  const content = data;
+  console.log(content.toString());
+});
